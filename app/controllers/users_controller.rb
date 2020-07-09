0992535e77a05ef1cc4d9530b7f7732d68d2e739
J Resilient <j.resilient.1@gmail.com>
2020-07-09 17:02:10 -0400

    Add code to pass specs plus #show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
-    
+    def new
+        render :new
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            redirect_to user_url(user)
+        else
+            flash.now[:errors] = user.errors.full_messages
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[id])
+        render :show
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
