b4e0b28f7f71abdba966bb2d398c35823dbfc3e8
J Resilient <j.resilient.1@gmail.com>
2020-07-09 17:24:22 -0400

    Add #logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user, :login!
+    helper_method :current_user, :login!, :logout
+    
     def current_user
         return nil if session[:session_token].nil?
         @current_user ||= User.find_by(session_token: session[:session_token])
@@ -8,5 +9,12 @@ class ApplicationController < ActionController::Base
     def login!(user)
         @current_user = user
         session[:session_token] = user.session_token
+    end
+
+    def logout!
+        return nil if @current_user.nil?
+        @current_user.reset_session_token!
+        @current_user = nil
+        session[:session_token] = nil
     end
 end
