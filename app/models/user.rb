244773808c553fd44f1b03f98f3e898b28ce895e
J Resilient <j.resilient.1@gmail.com>
2020-07-09 16:48:18 -0400

    Add code to pass tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,40 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+    attr_reader :password
+    after_initialize :set_session_token
+
+    validates :username, :password_digest, :session_token, presence: true
+    validates :username, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        token = self.class.generate_session_token
+        self.session_token = token
+        self.save!
+        token
+    end
+
+    def set_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
 
     def self.find_by_credentials(username, password)
-        
+        user = User.find_by(username: username)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
     end
 end
