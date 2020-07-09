b2df762159be56a4b2e23ee4077837311a93d861
J Resilient <j.resilient.1@gmail.com>
2020-07-09 17:06:31 -0400

    Add sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,15 @@
 class SessionsController < ApplicationController
-    
+    def new
+        render :show
+    end
+
+    def create
+        # validate credentials
+        # set session token
+    end
+
+    def destroy
+        # log user out
+        # reset session token
+    end
 end
