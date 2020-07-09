5508b8de5c07ab48ee9878bb5948e8f48a8e3331
J Resilient <j.resilient.1@gmail.com>
2020-07-09 15:10:05 -0400

    Add users model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+
+    def self.find_by_credentials(username, password)
+        
+    end
+end
