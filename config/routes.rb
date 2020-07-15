cf0959e91a1029069cf5fa9117c21e94c1eb6e6b
J Resilient <j.resilient.1@gmail.com>
2020-07-15 15:15:49 -0400

    Root to users index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
   resources :users, only: [:index, :new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
 end
