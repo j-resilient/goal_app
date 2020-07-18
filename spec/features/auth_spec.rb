221c80595f75027917663a93cd892a2c9193e58b
J Resilient <j.resilient.1@gmail.com>
2020-07-18 11:16:40 -0400

    Remove logout before each code in logging out feature as database_cleaner seems to be logging user out before each scenario anyway

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
-
 feature 'the signup process' do
+  
   scenario 'has a new user page' do
     visit new_user_url
     expect(page).to have_content "Sign Up!"
   end
-
+  
   feature 'signing up a user' do
     before(:each) do
       visit new_user_url
-      fill_in 'username', with: 'username'
-      fill_in 'password', with: 'password'
-      click_button 'Sign Up'
+      fill_in "Username", with: "username"
+      fill_in "Password", with: "password"
+      click_button "Sign Up"
     end
-
+    
     scenario 'shows username on the homepage after signup' do
       visit users_url
       expect(page).to have_content 'username'
     end
-
+    
   end
 end
 
 feature 'logging in' do
   subject(:user) { User.create!(username: "username", password: "password") }
-
+  
   before(:each) do
     visit new_session_url
     fill_in 'Username', with: user.username
@@ -42,10 +42,6 @@ end
 
 feature 'logging out' do
   let(:user) { User.create!(username: 'alice', password: 'password') }
-  before(:each) do
-    visit user_url(user)
-    click_button 'Log Out'
-  end
 
   scenario 'begins with a logged out state' do
     visit root_url
