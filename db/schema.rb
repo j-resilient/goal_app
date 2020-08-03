862c246508fb8ac89d7592877fdbc13f446758d4
J Resilient <j.resilient.1@gmail.com>
2020-08-03 14:12:27 -0400

    Add goal_comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_192250) do
+ActiveRecord::Schema.define(version: 2020_08_03_180819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goal_comments", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "goal_id", null: false
+    t.text "comment", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_goal_comments_on_author_id"
+    t.index ["goal_id"], name: "index_goal_comments_on_goal_id"
+  end
 
   create_table "goals", force: :cascade do |t|
     t.integer "user_id", null: false
