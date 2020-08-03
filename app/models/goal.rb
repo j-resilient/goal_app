0d198f0b1db09e46448029a3afe5fb0ada73edeb
J Resilient <j.resilient.1@gmail.com>
2020-08-03 14:18:47 -0400

    Add comments association

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -24,4 +24,10 @@ class Goal < ApplicationRecord
     end
 
     belongs_to :user
+
+    has_many :comments,
+        primary_key: :id,
+        foreign_key: :goal_id,
+        class_name: :GoalComment,
+        dependent: :destroy
 end
