e361b2408ec0ace5509a0dd3eac3947376184290
J Resilient <j.resilient.1@gmail.com>
2020-08-04 17:26:36 -0400

    Add test for goal comment creation

--- spec/features/goal_comments_spec.rb
+++ spec/features/goal_comments_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 feature 'goal comments' do
-    scenario 'create a comment' do
-        user = User.create!(username: 'a', password: 'password')
-        goal = Goal.create!(user_id: user.id, title: "testing testing")
+    let(:user) { User.create!(username: 'a', password: 'password') }
+    let(:goal) { Goal.create!(user_id: user.id, title: "testing testing") }
 
+    before(:each) do
+        visit new_session_url
+        fill_in 'username', with: user.username
+        fill_in 'password', with: user.password
+        click_button 'Log In'
+    end
+
+    scenario 'create a comment' do
         visit goal_url(goal)
         fill_in "comment", with: "this is a test comment"
         click_button "Save Comment"
