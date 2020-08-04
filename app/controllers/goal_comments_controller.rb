712ab379013d83fa04c6e1ffc0e233f26c22c432
J Resilient <j.resilient.1@gmail.com>
2020-08-04 17:28:05 -0400

    Refactor #create to be cleaner

--- app/controllers/goal_comments_controller.rb
+++ app/controllers/goal_comments_controller.rb
@@ -1,23 +1,11 @@
 class GoalCommentsController < ApplicationController
     def create
-        comment = GoalComment.new(
+        comment = GoalComment.create(
             author_id: current_user.id, 
-            goal_id: params[:id], 
+            goal_id: params[:goal_id], 
             comment: params[:comment])
 
-        unless comment.save
-            flash.now[:errors] = comment.errors.full_messages
-        end
-
-        redirect_to goal_url(params[:id])
-
-        # if comment.save
-        #     redirect_to goal_url(params[:id])
-        # else
-        #     flash.now[:errors] = comment.errors.full_messages
-        #     redirect_to goal_url(params[:id])
-        # end
+        flash.now[:errors] = comment.errors.full_messages
+        redirect_to goal_url(params[:goal_id])
     end
-
-    
 end
