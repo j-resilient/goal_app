bb4d1ae2b419add4cc64a2a965da200010707a99
J Resilient <j.resilient.1@gmail.com>
2020-08-05 17:35:43 -0400

    Add association validation for #comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,7 @@ RSpec.describe User, type: :model do
 
   describe 'associations' do
     it { should have_many(:goals) }
+    it { should have_many(:comments) }
   end
 
   describe '::find_by_credentials' do
