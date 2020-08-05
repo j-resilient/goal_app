dccb802ac5baa65fc6b64e36c175959c45dd5393
J Resilient <j.resilient.1@gmail.com>
2020-08-05 18:09:49 -0400

    Add validation tests

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
-    describe 'validations'
+    describe 'validations' do
+        it { should validate_presence_of(:user_id) }
+        it { should validate_presence_of(:title) }
+        it { should validate_length_of(:title).is_at_least(6) }
+    end
 
     describe 'associations'
 end
