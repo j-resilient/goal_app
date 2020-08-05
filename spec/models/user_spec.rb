800b287869f242c1d6b7e34af4996659099bfddb
J Resilient <j.resilient.1@gmail.com>
2020-08-05 18:04:48 -0400

    Change password validation test to check for the correct error message

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:username) }
-    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:password_digest).with_message("Password cannot be blank.") }
     it { should validate_presence_of(:session_token) }
 
     it { should validate_uniqueness_of(:username) }
