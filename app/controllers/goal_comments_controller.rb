865901c3504659d7b20edcf4e00e0954946570d3
J Resilient <j.resilient.1@gmail.com>
2020-08-05 16:38:28 -0400

    Switch to using polymorphic comments table

--- app/controllers/goal_comments_controller.rb
+++ app/controllers/goal_comments_controller.rb
@@ -1,9 +1,10 @@
 class GoalCommentsController < ApplicationController
     def create
-        comment = GoalComment.create(
+        comment = Comment.create(
             author_id: current_user.id, 
-            goal_id: params[:goal_id], 
-            comment: params[:comment])
+            commentable_type: "Goal",
+            commentable_id: params[:goal_id], 
+            comment_text: params[:comment])
 
         flash.now[:errors] = comment.errors.full_messages
         redirect_to goal_url(params[:goal_id])
--- app/controllers/user_comments_controller.rb
+++ app/controllers/user_comments_controller.rb
@@ -1,9 +1,10 @@
 class UserCommentsController < ApplicationController
     def create
-        comment = UserComment.create(
+        comment = Comment.create(
             author_id: current_user.id,
-            user_id: params[:user_id],
-            comment: params[:comment]
+            commentable_type: "User",
+            commentable_id: params[:user_id],
+            comment_text: params[:comment]
         )
         flash.now[:errors] = comment.errors.full_messages
         redirect_to user_url(params[:user_id])
