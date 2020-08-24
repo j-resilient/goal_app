4c65325a163f503f111ea86e21469824e742a81e
J Resilient <j.resilient.1@gmail.com>
2020-08-24 16:59:40 -0400

    Add POST #create tests to goals_controller_spec

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -16,7 +16,26 @@ RSpec.describe GoalsController, type: :controller do
         end
     end
 
-    describe 'POST #create'
+    describe 'POST #create' do
+        before(:each) do
+            allow(controller).to receive(:current_user) { user }
+        end
+
+        context 'with valid params' do
+            it 'redirects to show page' do
+                post :create, params: { goal: { title: "new goal"} }
+                expect(response).to redirect_to(goal_url(Goal.last))
+            end
+        end
+
+        context 'with invalid params' do
+            it 'validates that the title is at least 6 characters long' do
+                post :create, params: { goal: { title: "a" } }
+                expect(flash[:errors]).to be_present
+                expect(response).to render_template(:new)
+            end
+        end
+    end
 
     describe 'POST #update'
 
