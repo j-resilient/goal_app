06de4175c7c02b86eb940a53a7c17aacbcc942d8
J Resilient <j.resilient.1@gmail.com>
2020-08-24 16:49:46 -0400

    Add GET #new test to goals_controller_spec

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe GoalsController, type: :controller do
+    let(:user) { create(:user) }
+
     describe 'GET #new' do
-        it 'renders the new goal template'
+        context 'when logged in' do
+            before do
+                allow(controller).to receive(:current_user) { user }
+            end
+            it 'renders the new goal template' do
+                get :new, {}
+                expect(response).to render_template('new')
+                expect(response).to have_http_status(200)
+            end
+        end
     end
 
     describe 'POST #create'
