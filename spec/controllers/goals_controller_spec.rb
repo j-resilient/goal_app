e7f473ca1ff348dba9c38c00481cf88bdfa06958
J Resilient <j.resilient.1@gmail.com>
2020-08-24 15:20:58 -0400

    Add goals_controller_spec file

---
+++ spec/controllers/goals_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe GoalsController, type: :controller do
+    describe 'GET #new'
+
+    describe 'POST #create'
+
+    describe 'POST #update'
+
+    describe 'DELETE #destroy'
+end
